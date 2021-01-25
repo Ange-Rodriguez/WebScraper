db2f0e051db584f05e5527472b878be5c88954f6
Ange Rodriguez <ange.rodriguez@thunderwave.fr>
2021-01-25 19:46:58 +0100

    feat : ASCII Baner

--- index.js
+++ index.js
@@ -3,19 +3,32 @@ const puppeteer = require('puppeteer');
 const qoa = require('qoa');
 
 const isValidURL = (str) => (
-	str.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g) !== null
+	str.match(/(http(s)?:\/\/.)?(www\\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g) !== null
 );
 
 const prompt = async () => {
 	let input = await qoa.input({
-		query: 'Target URL:',
+		query: 'Please enter the target URL:',
 		handle: 'url'
 	});
 	if (isValidURL(input.url)) {
-			return input;
+		return input;
 	}
 	return await prompt();
 }
+
+console.log(`
+     _       __     __   _____                                
+    | |     / /__  / /_ / ___/______________ _____  ___  _____
+    | | /| / / _ \\/ __ \\\\__ \\/ ___/ ___/ __ \`/ __ \\/ _ \\/ ___/
+    | |/ |/ /  __/ /_/ /__/ / /__/ /  / /_/ / /_/ /  __/ /    
+    |__/|__/\\___/_.___/____/\\___/_/   \\__,_/ .___/\\___/_/     
+                                          /_/                                              
+=================================================================================
+    Version: 1.0.0    Date: 25/01/2021    Author: Ange Rodriguez
+=================================================================================
+`)
+
   
 prompt().then(async ({ url }) => {
 	const browser = await puppeteer.launch();
